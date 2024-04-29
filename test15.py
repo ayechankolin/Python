edf4177ffc3c5f07aec8e5471e89087b85005cc4
ayechankolin <ayechankolin2007@gmail.com>
2024-04-29 10:31:10 +0700

    test15.py

--- test15.py
+++ test15.py
@@ -1,10 +1,29 @@
-myfav_artists = ['PG', 'Kobe', 'Kyrie']
+# myfav_artists = ['PG', 'Kobe', 'Kyrie']
 
 # print(type(myfav_artists))
 # print(myfav_artists)
 # print(myfav_artists[1])
 # print(len(myfav_artists))
 
-print(myfav_artists[1:4])
-print(myfav_artists[1:])
-print(myfav_artists[:4])
+# print(myfav_artists[1:4])
+# print(myfav_artists[1:])
+# print(myfav_artists[:4])
+
+# print(myfav_artists)
+# myfav_artists.insert(0, 'KD')
+# print(myfav_artists)
+
+# myfav_artists.sort()
+# print(myfav_artists)
+
+# myfav_artists[0] = 'Paul George'
+# print(myfav_artists)
+
+artist_Tmac = {
+    'name' : 'Tracy Mac Grady',
+    'genre' : 'SG'
+}
+print(type(artist_Tmac))
+print(artist_Tmac['name'])
+artist_Tmac["Popular_move"] = "360 one handed dunk"
+print(artist_Tmac)
