dc6066dbab81fe7c6f35afd250402f863c66f742
ayechankolin <ayechankolin2007@gmail.com>
2024-04-29 09:53:38 +0700

    test15.py

---
+++ test15.py
@@ -0,0 +1,10 @@
+myfav_artists = ['PG', 'Kobe', 'Kyrie']
+
+# print(type(myfav_artists))
+# print(myfav_artists)
+# print(myfav_artists[1])
+# print(len(myfav_artists))
+
+print(myfav_artists[1:4])
+print(myfav_artists[1:])
+print(myfav_artists[:4])
